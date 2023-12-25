d0bcd7cb744e856523878acbf12eabc2c05cbc41
Ndzi Nfor <ndzinfor@gmail.com>
2023-12-25 09:40:25 +0100

    add elements using js

---
+++ src/page-load.js
@@ -0,0 +1,13 @@
+const content = document.getElementById("content");
+
+const header = document.createElement("div");
+header.setAttribute("id", "header");
+
+const cnt = document.createElement("div");
+cnt.setAttribute("id", "content-b");
+
+const testi = document.createElement("div");
+testi.setAttribute("id", "testimonial");
+
+const dImg = document.createElement("div");
+dImg.setAttribute("id", "d-img");
