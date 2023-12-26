ecb8a6f81a32aca7b64f310fe30ccef7e832de33
Ndzi Nfor <ndzinfor@gmail.com>
2023-12-26 14:33:43 +0100

    attach created elements to DOM

--- src/page-load.js
+++ src/page-load.js
@@ -6,7 +6,34 @@ export default function pageLoad() {
 
     const header = document.createElement("div");
     header.setAttribute("id", "header");
-    header.textContent = "Welcome to our Restaurant Page";
+    
+
+    const left = document.createElement("div")
+    left.setAttribute("id", "left");
+    left.textContent = "Welcome to our Restaurant Page";
+
+    const right = document.createElement("div")
+    right.setAttribute("id", "right");
+    
+
+    const home = document.createElement("button");
+    home.setAttribute("id", "home");
+    home.textContent = "Home"
+    right.appendChild(home);
+
+    const menu = document.createElement("button");
+    menu.setAttribute("id", "menu");
+    menu.textContent = "Menu"
+    right.appendChild(menu);
+
+    const contact = document.createElement("button");
+    contact.setAttribute("id", "contact");
+    contact.textContent = "Contact";
+    right.appendChild(contact);
+
+    header.appendChild(left);
+    header.appendChild(right);
+
 
     const cnt = document.createElement("div");
     cnt.setAttribute("id", "content-b");
