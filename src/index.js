365994b29245fb85b4bca217d3a36c98285c2dcf
Ndzi Nfor <ndzinfor@gmail.com>
2023-12-27 13:15:41 +0100

    import and call header in index file

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import pageLoad from './page-load.js';
 import menu from './Menu.js';
 import contact from './Contact.js';
 import './style.css';
+import Header from './Header';
 
+Header();
 pageLoad();
