087a4e0757f82b9baef8a9a63cfa25877941db47
Ndzi Nfor <ndzinfor@gmail.com>
2023-12-29 10:51:11 +0100

    add dummy content in menu page

--- src/Menu.js
+++ src/Menu.js
@@ -1,4 +1,20 @@
+import restauImg from './restau.jpg';
+
 
 export default function Menu() {
-    
+    const content = document.getElementById('content')
+
+    const txt = document.createElement('div');
+    txt.setAttribute('id', 'txt-1');
+    txt.textContent = "Crab & avocado bruschetta Lorem Ipsum is simply dummy text"
+
+    const dImg = document.createElement("div");
+    dImg.setAttribute("id", "d-img");
+
+    const imgIcn = new Image();
+    imgIcn.src = restauImg;
+    dImg.appendChild(imgIcn);
+
+    content.appendChild(txt);
+    content.appendChild(dImg);
 }
