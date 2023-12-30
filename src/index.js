b333d651c34f977187713a266551a313e4819017
Ndzi Nfor <ndzinfor@gmail.com>
2023-12-30 14:33:46 +0100

    implement tab switching login in index page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import pageLoad from './page-load.js';
-import contact from './Contact.js';
+import Menu from './Menu.js';
+import Contact from './Contact.js';
+import Header from './Header.js';
 import './style.css';
 
-pageLoad();
+document.addEventListener('DOMContentLoaded', () => {
+    const content = document.getElementById('content');
+    content.appendChild(Header());
+
+    // Default to home page on initial load
+    pageLoad();
+
+    // Tab switching logic
+    document.getElementById('home').addEventListener('click', pageLoad);
+    document.getElementById('menu').addEventListener('click', Menu);
+    document.getElementById('contact').addEventListener('click', Contact);
+});
