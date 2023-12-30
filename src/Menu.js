d3f7be9d1112ce970b88bcfd31fe849271e406c7
Ndzi Nfor <ndzinfor@gmail.com>
2023-12-30 06:01:25 +0100

    Update menu page to display 3 menu

--- src/Menu.js
+++ src/Menu.js
@@ -6,19 +6,59 @@ export default function Menu() {
     content.innerHTML = ''; // Clear existing content
     content.appendChild(Header()); // Add header
 
-    const txt = document.createElement('div');
-    txt.setAttribute('id', 'txt-1');
-    txt.textContent = "Crab & avocado bruschetta Lorem Ipsum is simply dummy text"
+    // Creating main container for the menu page
+    const menuContainer = document.createElement('div');
+    menuContainer.setAttribute('id', 'menu-container');
+    menuContainer.style.display = 'flex';
+    menuContainer.style.flexDirection = 'column';
+    menuContainer.style.alignItems = 'center';
+    menuContainer.style.justifyContent = 'center';
+    menuContainer.style.margin = '20px';
+    menuContainer.style.padding = '20px';
+    menuContainer.style.borderRadius = '10px';
+    menuContainer.style.backgroundColor = '#f3f3f3';
 
-    const dImg = document.createElement("div");
-    dImg.setAttribute("id", "d-img");
+    // Menu items
+    const dishes = [
+        { name: 'Dish 1', description: 'Description of Dish 1' },
+        { name: 'Dish 2', description: 'Description of Dish 2' },
+        { name: 'Dish 3', description: 'Description of Dish 3' }
+    ];
 
-    const imgIcn = new Image();
-    imgIcn.src = restauImg;
-    dImg.appendChild(imgIcn);
+    dishes.forEach(dish => {
+        // Dish container
+        const dishContainer = document.createElement('div');
+        dishContainer.style.marginBottom = '20px';
 
-    content.appendChild(txt);
-    content.appendChild(dImg);
+        // Dish name
+        const dishName = document.createElement('h2');
+        dishName.textContent = dish.name;
+        dishName.style.color = '#333';
+
+        // Dish description
+        const dishDescription = document.createElement('p');
+        dishDescription.textContent = dish.description;
+        dishDescription.style.textAlign = 'center';
+
+        // Dish image
+        const image = new Image();
+        image.src = restauImg;
+        image.style.width = '200px';
+        image.style.height = '150px';
+        image.style.borderRadius = '10px';
+        image.style.marginTop = '10px';
+
+        // Appending elements to the dish container
+        dishContainer.appendChild(dishName);
+        dishContainer.appendChild(dishDescription);
+        dishContainer.appendChild(image);
+
+        // Appending the dish container to the menu container
+        menuContainer.appendChild(dishContainer);
+    });
+
+    // Appending the menu container to the content div
+    content.appendChild(menuContainer);
 }
 
 
