a3ddad471e38d5bccb2bcb21f03588b0cd6bf678
Ndzi Nfor <ndzinfor@gmail.com>
2023-12-30 06:14:16 +0100

    Update look of home page

--- src/page-load.js
+++ src/page-load.js
@@ -7,25 +7,44 @@ export default function pageLoad() {
 
     content.appendChild(Header()); // Add header
 
-    const cnt = document.createElement("div");
-    cnt.setAttribute("id", "content-b");
+    // Creating main container for the home page
+    const homeContainer = document.createElement('div');
+    homeContainer.setAttribute('id', 'home-container');
+    homeContainer.style.display = 'flex';
+    homeContainer.style.flexDirection = 'column';
+    homeContainer.style.alignItems = 'center';
+    homeContainer.style.justifyContent = 'center';
+    homeContainer.style.margin = '20px';
+    homeContainer.style.padding = '20px';
+    homeContainer.style.borderRadius = '10px';
+    homeContainer.style.backgroundColor = '#f3f3f3';
 
-    const testi = document.createElement("div");
-    testi.setAttribute("id", "testimonial");
-    testi.innerText = "They got really good food. Despite my being a foodie, the taste of their food is out of this world";
-    cnt.appendChild(testi);
+    // Welcome message
+    const welcomeMsg = document.createElement('h1');
+    welcomeMsg.textContent = 'Welcome to Our Restaurant';
+    welcomeMsg.style.color = '#333';
+    welcomeMsg.style.marginBottom = '15px';
 
-    const dImg = document.createElement("div");
-    dImg.setAttribute("id", "d-img");
-    
-    const myIcon = new Image();
-    myIcon.src = restauImg;
-    dImg.appendChild(myIcon);
+    // Introduction text
+    const introText = document.createElement('p');
+    introText.innerHTML = 'Enjoy the finest culinary experiences with our exclusive dishes and ambient atmosphere.';
+    introText.style.textAlign = 'center';
 
-    cnt.appendChild(dImg);
+    // Restaurant image
+    const image = new Image();
+    image.src = restauImg;
+    image.style.width = '300px';
+    image.style.height = '200px';
+    image.style.borderRadius = '10px';
+    image.style.marginTop = '20px';
 
-    content.appendChild(header);
-    content.appendChild(cnt);
+    // Appending elements to the home container
+    homeContainer.appendChild(welcomeMsg);
+    homeContainer.appendChild(introText);
+    homeContainer.appendChild(image);
+
+    // Appending the home container to the content div
+    content.appendChild(homeContainer);
 }
 
 
